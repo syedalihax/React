a2583536575600695aa47bd9a4fb13f20104e683
SyedAli-Developer <syedali.h.b.g.626@gmail.com>
2026-05-12 14:16:09 +0500

    stop practing and go to take some rest

--- Notes-App/src/App.jsx
+++ Notes-App/src/App.jsx
@@ -1,121 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import React, { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [title, setTitle] = useState('')
+  const [detail, setDetail] = useState('')
+  const submitHandler = (e) => {
+    console.log(title)
+    setTitle('')
+    console.log(detail)
+    setDetail('')
+  }
+  const handleInput = (e) => {
+    const element = e.target;
+    element.style.height = "auto";
+    element.style.height = `${element.scrollHeight}px`;
+  };
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className='bg-black h-screen text-white flex flex-col'>
 
-      <div className="ticks"></div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        submitHandler(e)
+      }} className='flex w-full sm:w-11/12 md:w-10/12 md:max-w-175 mx-auto  flex-col gap-5 p-5 sm:p-8'>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+
+        <input type="text"
+          placeholder='Title'
+          value={title}
+          onChange={(e)=>{
+            setTitle(e.target.value)
+          }}
+          className=' outline-none border rounded-[10px] py-2.5 px-5' />
+
+
+        <textarea type="text"
+          onChange={handleInput}
+          value={detail}
+          onch
+          placeholder='Write Details'
+          className='outline-none border rounded-[10px] py-3 px-5  overflow-hidden resize-none' >
+        </textarea>
+
+
+        <button className='border border-white bg-white text-black py-3 rounded-[10px] overflow-auto resize-none font-bold sm:text-2xl'>Add Note</button>
+
+
+      </form>
+
+
+      <div className='bg-amber-800 py-5 px-5 sm:px-10 md:px-10 w-full flex gap-y-8 gap-x-3 flex-wrap justify-evenly max-h-[80vh] overflow-y-auto custom-scrollbar'>
+
+        <div className='text-black p-5 bg-gray-300 w-60 sm:w-60 md:w-75 min-h-50 border-none rounded-[10px]'>
+          <h1 className='sm:text-3xl border-b-2 p-1 mb-4'>Title</h1>
+          <p className='sm:text-lg'>Description...</p>
+        </div>
+
+        <div className='text-black p-5 bg-gray-300 w-60 sm:w-60 md:w-75 min-h-50 border-none rounded-[10px]'>
+          <h1 className='sm:text-3xl border-b-2 p-1 mb-4'>Title</h1>
+          <p className='sm:text-lg'>Description...</p>
+        </div>
+
+        <div className='text-black p-5 bg-gray-300 w-60 sm:w-60 md:w-75 min-h-50 border-none rounded-[10px]'>
+          <h1 className='sm:text-3xl border-b-2 p-1 mb-4'>Title</h1>
+          <p className='sm:text-lg'>Description...</p>
+        </div>
+        
+       
+        
+       
+        
+        
+      </div>
+     
+    </div >
+    
   )
 }
 
