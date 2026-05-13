c60b973541e7f1901200bc30ea98c5351917b853
SyedAli-Developer <syedali.h.b.g.626@gmail.com>
2026-05-13 08:40:52 +0500

    Start practicing

--- Notes-App/src/Components/Card.jsx
+++ Notes-App/src/Components/Card.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { X } from 'lucide-react'; 
 
 const Card = (props) => {
   return (
-     <div className="text-black bg-gray-300 p-5 w-60 sm:w-60 md:w-75 min-h-50 border-none rounded-[10px] overflow-hidden">
-
+     <div className="relative text-black bg-gray-300 p-5 w-60 sm:w-60 md:w-75 min-h-50 border-none rounded-[10px] overflow-hidden">
+          <X className='absolute right-5 top-7 hover:bg-gray-400 cursor-pointer w-8 h-8 p-1 bg-gray-300 rounded-2xl'/>
           <h1 className='text-2xl sm:text-3xl border-b-2 p-1 mb-4 overflow-hidden wrap-normal whitespace-pre-wrap'>{props.title}</h1>
           <p className='sm:text-lg wrap-anywhere whitespace-pre-wrap'>{props.detail}</p>
         </div>
