068b574afc89f45fb9a31590c45fff679c543659
syedalihax <syedalihax@gmail.com>
2026-07-12 10:07:26 -0700

    clear

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -3,7 +3,19 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <div>App</div>
+      <Card
+        name="Ali"
+        age={22}
+        city="Karachi"
+      />
+
+
+    </>
+
+
+
   )
 }
 
