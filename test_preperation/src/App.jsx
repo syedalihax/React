537a6bee34d2e8597a59f055c4bed81946dcc561
syedalihax <syedalihax@gmail.com>
2026-07-13 08:27:35 -0700

    Practice : conditional rendering

--- test_preperation/src/App.jsx
+++ test_preperation/src/App.jsx
@@ -1,63 +1,22 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [name, setName] = useState('')
-  const [Age, setAge] = useState('')
-  const [City, setCity] = useState('')
-  const [newName, setNewName] = useState('')
-  const [newAge, setNewAge] = useState('')
-  const [newCity, setNewCity] = useState('')
-
+  const [visibility , setVisibility] = useState(true)
   return (
     <>
-      <form onSubmit={(e) => {
-        e.preventDefault()
-        setNewName(name)
-        setNewAge(Age)
-        setNewCity(City)
-        
-      }}>
-        <label htmlFor="Name">Name:</label>
-
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value)
-          }}
-          required
-        />
-        <br /><br />
-        <label htmlFor="Name">Age:</label>
-
-        <input
-          type="number"
-          value={Age}
-          onChange={(e) => {
-            setAge(e.target.value)
-          }}
-          required
-        />
-        <br /><br />
-        <label htmlFor="Name">City:</label>
-
-        <input
-          type="text"
-          value={City}
-          onChange={(e) => {
-            setCity(e.target.value)
-          }}
-          required
-        />
-        <br /><br />
-        <input type="submit" />
-
-      </form>
-
-      <h1>Name: {newName}</h1>
-      <h1>Age: {newAge}</h1>
-      <h1>City: {newCity}</h1>
-
+    {visibility ? <h1>Hello Syed Ali</h1> : <h2>Content Hide</h2>}
+    <br />
+    
+    {visibility ? <button onClick={()=>{
+      setVisibility(!visibility)
+    }}>Hide</button>
+  :
+  <button
+  onClick={()=>{
+    setVisibility(!visibility)
+  }}
+  >Show</button>
+  }
     </>
 
   )
