b316ffa48d163d6b141a07bda669d0e988805420
syedalihax <syedalihax@gmail.com>
2026-07-13 09:27:00 -0700

    Practice : React mapping

--- test_preperation/src/App.jsx
+++ test_preperation/src/App.jsx
@@ -1,22 +1,44 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [visibility , setVisibility] = useState(true)
+  const students = [
+    {
+      id: 1,
+      name: "Ali",
+      age: 22,
+      city: "Karachi"
+    },
+
+    {
+      id: 2,
+      name: "Ahmed",
+      age: 20,
+      city: "Lahore"
+    },
+
+    {
+      id: 3,
+      name: "Hamza",
+      age: 24,
+      city: "Quetta"
+    }
+
+  ]
   return (
     <>
-    {visibility ? <h1>Hello Syed Ali</h1> : <h2>Content Hide</h2>}
-    <br />
-    
-    {visibility ? <button onClick={()=>{
-      setVisibility(!visibility)
-    }}>Hide</button>
-  :
-  <button
-  onClick={()=>{
-    setVisibility(!visibility)
-  }}
-  >Show</button>
-  }
+      <h1>Students</h1>
+      <div>
+      {students.map((student)=>{
+        return (
+          <div key={student.id}>
+            <h1>Name: {student.name}</h1>
+            <h1>Role: {student.id}</h1>
+            <h1>Age : {student.age}</h1>
+            <h1>City: {student.city}</h1>
+          </div>
+        )
+      })}
+      </div>
     </>
 
   )
