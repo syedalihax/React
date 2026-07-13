6f984382836ae696eeaf7d5bcd1c659374d5c20c
syedalihax <syedalihax@gmail.com>
2026-07-13 09:50:34 -0700

    practice : UseEffect hook

--- test_preperation/src/App.jsx
+++ test_preperation/src/App.jsx
@@ -1,44 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
-  const students = [
-    {
-      id: 1,
-      name: "Ali",
-      age: 22,
-      city: "Karachi"
-    },
-
-    {
-      id: 2,
-      name: "Ahmed",
-      age: 20,
-      city: "Lahore"
-    },
-
-    {
-      id: 3,
-      name: "Hamza",
-      age: 24,
-      city: "Quetta"
-    }
-
-  ]
+  const [name, setName] = useState("");
+  useEffect(()=>{
+    console.log(name)
+  }, [name])
   return (
     <>
-      <h1>Students</h1>
-      <div>
-      {students.map((student)=>{
-        return (
-          <div key={student.id}>
-            <h1>Name: {student.name}</h1>
-            <h1>Role: {student.id}</h1>
-            <h1>Age : {student.age}</h1>
-            <h1>City: {student.city}</h1>
-          </div>
-        )
-      })}
-      </div>
+    <input type="text" value={name} placeholder='Enter Text...'
+    
+    onChange={(e)=>{
+      setName(e.target.value)
+    }}
+    />
+    
     </>
 
   )
