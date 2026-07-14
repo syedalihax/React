9ff62a399a10c7034b620fede835cf53facbec80
syedalihax <syedalihax@gmail.com>
2026-07-13 21:02:08 -0700

    practice : async & await

--- test_preperation/Project-01/src/App.jsx
+++ test_preperation/Project-01/src/App.jsx
@@ -1,31 +1,36 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-
+import React from 'react'
+import { useState, useEffect } from 'react'
 const App = () => {
-
   const [user, setUser] = useState([])
+
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response)=>{
-      return response.json()
-    }).then((data)=>{
-      setUser(data)
-    })
+
+    const getUsers = async () => {
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+    const data = await response.json()
     
+    setUser(data)
+
+  }
+  getUsers()  
+   
   }, [])
-  
+
+
   return (
     <>
-    {user.map((userdata)=>{
-      return (
-        <div key={userdata.id}>
-        <hr />
-        <h2>{userdata.name}</h2>
-        <h2>User Name: {userdata.username}</h2>
-        <h3>Email: {userdata.email}</h3>
-      </div>
-      )
-    })}
+      {user.map((userdata)=>{
+        return(
+          <div key={userdata.id}>
+            <hr />
+            <h2>{userdata.name}</h2>
+            <h2>{userdata.username}</h2>
+            <h2>Email: {userdata.email}</h2>
+          </div>
+        )
+      })}
     </>
   )
 }
