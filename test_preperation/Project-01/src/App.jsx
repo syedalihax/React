3c681df48f4f7031191b5bae5b38d99149422a88
syedalihax <Syedalihax@gmail.com>
2026-07-15 09:40:04 +0500

    react test complete react end for now

--- test_preperation/Project-01/src/App.jsx
+++ test_preperation/Project-01/src/App.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    let timer
+    const getPosts = async () => {
+      try {
+        
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        setPosts(response.data)
+        
+      } catch (error) {
+        setError(error.message)
+      } finally{
+        timer = setTimeout(() => {
+          
+        setLoading(false)
+        }, 1000);
+      }
+    }
+    getPosts()
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
+
+  if (loading) return <h1>Loading...</h1>
+  if (error) return <h1>{error}</h1>
   return (
-    <div>
-      
+
+    <>
+{posts.map((post)=>{
+  return(
+
+    <div key={post.id}>
+      <hr />
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
     </div>
+  )
+})}
+    </>
   )
 }
 
