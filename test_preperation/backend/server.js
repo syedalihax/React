2f219f8e3d498137de95ec10cfd3db06d1e6dde6
syedalihax <Syedalihax@gmail.com>
2026-07-18 21:14:35 +0500

    Practice : HTTP Method put and Delete

--- test_preperation/backend/server.js
+++ test_preperation/backend/server.js
@@ -1,49 +1,151 @@
-const express = require("express")
-const app = express()
-const mongoose = require("mongoose")
+const express = require("express");
+const mongoose = require("mongoose");
 
-app.use(express.json())
+const app = express();
 
-app.get("/" , (req , res)=>{
-    res.send("Home Page")
-})
+app.use(express.json());
+
 
 mongoose.connect("mongodb://127.0.0.1:27017/backend_practice")
-.then(() => {
-    console.log("MongoDB Connected");
-})
-.catch((err) => {
-    console.log(err);
-});
+    .then(() => {
+        console.log("MongoDB Connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
-const studentSchema = new mongoose.Schema({
-    name: String,
-    age: Number
-});
 
-const Student = mongoose.model("Student", studentSchema);
+
+// =======================
+// RULES
+// =======================
+
+const Rules = new mongoose.Schema(
+    {
+
+        name: String,
+
+        age: Number
+
+    },
+    {
+        collection: "StudentsCollection"
+    }
+);
+
+
+
+// =======================
+// WORKER
+// =======================
+
+const Model = mongoose.model("Student", Rules);
+
+
+
+// =======================
+// ROUTE
+// =======================
 
 app.post("/students", async (req, res) => {
+
     try {
 
-        const student = await Student.create(req.body);
+        const Created = await Model.create(req.body);
 
         res.status(201).json({
+
             success: true,
+
             message: "Student Created Successfully",
-            data: student
-        });
 
-    } catch (error) {
+            data: Created
 
-        res.status(500).json({
-            success: false,
-            message: error.message
         });
 
     }
+
+    catch (error) {
+
+        res.status(500).json({
+
+            success: false,
+
+            message: error.message
+
+        });
+
+    }
+
 });
 
-app.listen(3000 , (()=>{
-    console.log("server is running on port 3000")
-}))
+app.get("/students", async (req, res) => {
+
+    try {
+
+        const documents = await Model.find();
+
+        res.status(200).json({
+
+            success: true,
+
+            message: "All Student documents collected Successfully",
+
+            data: documents
+
+        });
+
+    }
+
+    catch (error) {
+
+        res.status(500).json({
+
+            success: false,
+
+            message: error.message
+
+        });
+
+    }
+
+});
+
+app.put("/students/:id", async (req, res) => {
+    try {
+        const Updated = await Model.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        res.status(200).json({
+            success : true,
+            message : "Student Updated Succcesfully",
+            data: Updated
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+
+})
+
+app.delete("/students/:id", async (req, res) => {
+    try {
+        const Deleted = await Model.findByIdAndDelete(req.params.id)
+        res.status(200).json({
+            success : true,
+            message : "Student Removed Succcesfully",
+            data: Deleted
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+
+})
+
+app.listen(3000, () => {
+    console.log("Server runs https://localhost:3000");
+
+});
